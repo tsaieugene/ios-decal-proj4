ef872cf8c3e2127c1fc7d37a513860e42e21db2d
AlexH1007 <alexhuang@Alexs-Mac-Pro.local>
2016-05-01 21:53:03 -0700

    added eugene's part

--- SCloudNine/SCloudNine/PlaylistViewController.swift
+++ SCloudNine/SCloudNine/PlaylistViewController.swift
@@ -5,13 +5,149 @@
 //  Created by Eddy Kim on 4/24/16.
 //  Copyright © 2016 iOSDecal. All rights reserved.
 //
-
+//
 import Foundation
 import UIKit
 
-class PlaylistViewController: UIViewController {
+class PlaylistViewController: UITableViewController {
+    
+    var songs = ["song 1", "song 2", "song 3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 0.914, green: 0.918, blue: 0.918, alpha: 1)
+
+        tableView.registerClass(MyCell.self, forCellReuseIdentifier: "cellId")
+        tableView.registerClass(Header.self, forHeaderFooterViewReuseIdentifier: "headerId")
+        
+        tableView.sectionHeaderHeight = 50
+        
     }
+    
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCellWithIdentifier("cellId", forIndexPath: indexPath) as! MyCell
+        myCell.nameLabel.text = songs[indexPath.row]
+        myCell.playlistViewController = self
+        return myCell
+    }
+    
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return tableView.dequeueReusableHeaderFooterViewWithIdentifier("headerId")
+    }
+    
+    func deleteCell(cell: UITableViewCell) {
+        if let deletionIndexPath = tableView.indexPathForCell(cell) {
+            songs.removeAtIndex(deletionIndexPath.row)
+            tableView.deleteRowsAtIndexPaths([deletionIndexPath], withRowAnimation: .Automatic)
+        }
+    }
+    
+}
+
+class Header: UITableViewHeaderFooterView {
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Playlist"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFontOfSize(24)
+        return label
+    }()
+    
+    func setupViews() {
+        addSubview(nameLabel)
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-16-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        
+    }
+    
+}
+
+class MyCell: UITableViewCell {
+    
+    var playlistViewController: PlaylistViewController?
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = ""
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFontOfSize(14)
+        return label
+    }()
+    
+    let deleteButton: UIButton = {
+        let button = UIButton(type: .System)
+        button.setTitle("Delete", forState: .Normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let voteButton: UIButton = {
+        let button = UIButton(type: .System)
+        button.setTitle("Vote", forState: .Normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let votesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFontOfSize(14)
+        return label
+    }()
+    
+    func setupViews() {
+        addSubview(nameLabel)
+        addSubview(deleteButton)
+        addSubview(voteButton)
+        addSubview(votesLabel)
+        
+        deleteButton.addTarget(self, action: "deleteSong", forControlEvents: .TouchUpInside)
+        voteButton.addTarget(self, action: "voteSong", forControlEvents: .TouchUpInside)
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-16-[v0]-8-[v1(80)]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel, "v1": deleteButton]))
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-16-[v0]-8-[v1(80)]-60-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel, "v1": voteButton]))
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-16-[v0]-8-[v1(80)]-90-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel, "v1": votesLabel]))
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": deleteButton]))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": voteButton]))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": votesLabel]))
+    }
+    
+    func deleteSong() {
+        playlistViewController?.deleteCell(self)
+    }
+    
+    func voteSong() {
+        
+
+    }
+    
 }
